1eaf88349dc854b519903f51dc206754b48f8aa9
Paul Williams <paul@paulswilliams.me.uk>
2016-08-02 14:17:39 +0100

    basic iOS app with Calabash server linked

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text="Default Text" 
+    }
 }
 
